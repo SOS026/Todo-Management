3d9ae497f96e10ed254d09864647f735148eb96d
Sourabh Soni <sourabh.soni@cubastion.com>
2024-02-19 13:07:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TodoComponent } from './pages/todo/todo.component';
+import { TodoCardComponent } from './shared/components/todo-card/todo-card.component';
+import { MasterComponent } from './shared/layouts/master/master.component';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { DefaultComponent } from './shared/layouts/default/default.component';
+import { AlertComponent } from './shared/ui/alert/alert.component';
+import { SlidePanelComponent } from './shared/ui/slide-panel/slide-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TodoComponent,
+    TodoCardComponent,
+    // MasterComponent,
+    // HeaderComponent,
+    // DefaultComponent,
+    AlertComponent,
+    SlidePanelComponent
   ],
   imports: [
     BrowserModule,
